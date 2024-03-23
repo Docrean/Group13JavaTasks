eb0ac7a327d6126ab7ca228a420812acc7317545
Lilluchee <lilluchee@gmail.com>
2024-03-23 18:09:46 -0500

    Odd or Even

---
+++ src/Lily/OddOrEven.java
@@ -0,0 +1,15 @@
+package Lily;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int num1 = 150;
+
+        if(num1 % 2 == 0){
+            System.out.println(num1 + " is even number");
+        }else{
+            System.out.println(num1 + " is odd number");
+        }
+    }
+}
