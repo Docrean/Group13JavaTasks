e521bad2fcc8de18bdee119c20d501215d153f31
Docrean <bverns88@gmail.com>
2024-03-27 10:30:14 -0700

    Update

---
+++ src/Brandon/Wk1/Tasks.java
@@ -0,0 +1,17 @@
+package Brandon.Wk1;
+
+public class Tasks {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+    /*
+    Write a method that indentifies if a given number is even or odd:
+                Indentify(5)->"odd"
+                Indentify(6)->"even"
+
+     */
