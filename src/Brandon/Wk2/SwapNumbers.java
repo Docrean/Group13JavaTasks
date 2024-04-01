775d34af24a3e1228b5431725198486d1cf27813
DocBFxClass <bverns88@gmail.com>
2024-03-31 20:41:21 -0700

    UPDATE

---
+++ src/Brandon/Wk2/SwapNumbers.java
@@ -0,0 +1,18 @@
+package Brandon.Wk2;
+
+public class SwapNumbers {
+
+    //Swap two variables' values without using a third variable
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        num1 = num2; //num1 now 20
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
